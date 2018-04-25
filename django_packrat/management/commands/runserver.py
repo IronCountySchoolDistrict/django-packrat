124fad7035463bff3e83babe23ea1eff9001fc70
Nathan Jones <nathan.jones@ironmail.org>
2018-04-25 14:05:25 -0600

    Reference stdout, stderr streams the way Django 2.0 wants us to

--- django_packrat/management/commands/runserver.py
+++ django_packrat/management/commands/runserver.py
@@ -109,8 +109,8 @@ class Command(StaticfilesRunserverCommand):
             webpack_command,
             shell=True,
             stdin=subprocess.PIPE,
-            stdout=self.stdout,
-            stderr=self.stderr
+            stdout=self.stdout._out,
+            stderr=self.stderr._out
         )
 
         if self.webpack_process.poll() is not None:
@@ -134,8 +134,8 @@ class Command(StaticfilesRunserverCommand):
             browsersync_command,
             shell=True,
             stdin=subprocess.PIPE,
-            stdout=self.stdout,
-            stderr=self.stderr
+            stdout=self.stdout._out,
+            stderr=self.stderr._out
         )
 
         if self.browsersync_process.poll() is not None:
